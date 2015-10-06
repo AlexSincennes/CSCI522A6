e5f55e7e2fa340fc7f3e7d3f7b58e315e6932720
Sajini <sajini_pm@outlook.com>
2015-10-05 20:33:55 -0700

    add assetsout for michael & the a6 level

---
+++ AssetsOut/MichealPackage/Materials/Michael_Geo1_Michael_Col1.lua
@@ -0,0 +1,24 @@
+function fillMaterialTable(args) -- the script format requires existence of this function
+args['version']=2
+args['diffuse']={1.000000, 1.000000, 1.000000, 1.000000}
+args['shininess']=5.000000
+args['reflectivity']={0.000000, 0.000000, 0.000000}
+args['specular']={0.200000, 0.200000, 0.200000}
+args['emissive']={0.400000, 0.400000, 0.400000}
+args['textures']={
+  {'NORMAL', 'michael_diff.png'},
+  {'SPECULAR', 'michael_diff.png'},
+  {'GLOW', 'michael_diff.png'},
+} -- end textures
+--notes overrides
+args['textures']={
+{'COLOR', 'michael_diff.dds'},
+{'SPECULAR', 'michael_diff.dds'},
+{'NORMAL', 'michael_diff.dds'},
+{'GLOW', 'michael_diff.dds'},
+} -- end textures
+--notes overrides
+
+--notes end
+
+end
